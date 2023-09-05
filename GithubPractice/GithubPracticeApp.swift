a405773ea39a2ea506755a66d7ad83c1a16fcc8f
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2023-09-05 19:34:26 +0900

    Initial Commit

---
+++ GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by CNU on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
